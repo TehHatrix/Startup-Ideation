2a5578150a5a3114ff5b8099e653ef2e64e889b8
Wan Nasrul Irfan <nasrulirfan02@gmail.com>
2021-11-08 02:11:31 +0800

    Added Font Awesome Support

--- vue-ideation/src/main.js
+++ vue-ideation/src/main.js
@@ -4,12 +4,21 @@ import router from './router'
 import store from './store'
 import '@/assets/style.css'
 import Axios from 'axios'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import vSelect from 'vue-select'
 // import {vsSelect } from 'vuesax'
 // import 'vuesax/dist/vuesax.css'
 
 // Vue.component('v-select', vSelect)
 // Vue.use(vsSelect)
+/* add icons to the library */
+library.add(faUserSecret)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 Vue.prototype.$http = Axios;
 
