396b475c91f88036523ad331921f335747e0ed03
Wan Nasrul Irfan <nasrulirfan02@gmail.com>
2021-11-12 01:28:00 +0800

    Updated Customer Card

--- vue-ideation/src/main.js
+++ vue-ideation/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import '@/assets/style.css'
 import Axios from 'axios'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faStar,faCircleExclamation,faUsers, faBullseye, faScroll, faListCheck } from '@fortawesome/free-solid-svg-icons'
+import { faStar,faCircleExclamation,faUsers, faBullseye, faScroll, faListCheck, faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
 // import { faCirclePlus } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // import vSelect from 'vue-select'
@@ -15,7 +15,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 // Vue.component('v-select', vSelect)
 // Vue.use(vsSelect)
 /* add icons to the library */
-library.add(faStar,faCircleExclamation,faUsers,faBullseye,faScroll,faListCheck)
+library.add(faStar,faCircleExclamation,faUsers,faBullseye,faScroll,faListCheck,faCheck,faXmark)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
