5dfa8207499b2d2c8d65ac5273288ef4519e46c2
Wan Nasrul Irfan <nasrulirfan02@gmail.com>
2021-11-13 23:14:45 +0800

    Gradient Icons

---
+++ vue-ideation/src/components/icons/x-mark.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="xmark"
+    class="svg-inline--fa fa-xmark"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 320 512"
+  >
+    <defs>
+      <linearGradient id="GradientColor" x1="0%" y1="0%" x2="0%" y2="100%">
+        <stop offset="0%" stop-color="#8743FF" />
+        <stop offset="100%" stop-color="#4136F1" />
+      </linearGradient>
+    </defs>
+    <path
+      fill="url(#GradientColor)"
+      d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
+    ></path>
+  </svg>
+</template>
+
+<style scoped>
+svg{
+    height:50px;
+    width:50px;
+    font-weight:800;
+}
+</style>
