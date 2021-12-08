a1c0c97ce89921e74571d3eb81423b08973255b4
Wan Nasrul Irfan <nasrulirfan02@gmail.com>
2021-12-08 17:27:51 +0800

    Updated

--- vue-ideation/src/views/Survey/SurveyDashboard.vue
+++ vue-ideation/src/views/Survey/SurveyDashboard.vue
@@ -14,26 +14,26 @@
           <template #logo>
             <sign-up :gradientBoolean="true"></sign-up
           ></template>
-          <template #title> <h3>Answered Survey</h3></template>
+          <template #title> <h4>Answered Survey</h4></template>
           <template #content> <h1>17</h1> </template>
         </dashboard-card>
         <dashboard-card>
           <template #logo> <eyes :gradientBoolean="true"></eyes></template>
-          <template #title> <h3>Total Unique Views</h3></template>
+          <template #title> <h4>Total Unique Views</h4></template>
           <template #content> <h1>130</h1> </template>
         </dashboard-card>
         <dashboard-card>
           <template #logo>
             <revenue :gradientBoolean="true"> </revenue
           ></template>
-          <template #title> <h3>Follow Up Emails</h3></template>
+          <template #title> <h4>Follow Up Emails</h4></template>
           <template #content> <h1>150</h1> </template>
         </dashboard-card>
         <dashboard-card>
           <template #logo>
             <revenue-target :gradientBoolean="true"></revenue-target>
           </template>
-          <template #title> <h3>Follow Up Goal Emails</h3></template>
+          <template #title> <h4>Follow Up Goal Emails</h4></template>
           <template #content> <h1>200</h1> </template>
         </dashboard-card>
       </div>
