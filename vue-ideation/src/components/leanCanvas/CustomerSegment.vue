cdcd48dac74cd2f60271a3115fcfd092a852f444
syaqil520 <syaqil520@gmail.com>
2022-01-31 23:36:44 +0800

    before integrate

---
+++ vue-ideation/src/components/leanCanvas/CustomerSegment.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div>
+        <div class="c-body-card">
+            <div class="c-body-title " >
+                Customer Segment
+            </div>
+            <div class="c-body-content">
+                <div class="general-button">
+                    NEW
+                </div>
+            </div>
+        </div>
+
+        <modal
+            :showModal="showAddModal"
+            @close="closeAddModal" >
+
+            <h2>Add Customer Segment</h2>
+
+        </modal>
+    </div>
+</template>
+<script>
+import ProjectModalVue from '../ProjectModal.vue'
+// import api from '../../api/leanCanvasApi'
+
+export default {
+    name: "CustomerSegment",
+    components: {
+        'modal': ProjectModalVue
+    },
+
+    data() {
+        return {
+            showAddModal: false
+        }
+    },
+
+    methods: {
+        closeAddModal() {
+
+        }
+    }
+
+
+    
+}
+</script>
+<style lang="">
+    
+</style>
